a0b2228209c86483a4aa3bad2d8e27fd4e65feb6
greenorange1994 <yz3045@columbia.edu>
2017-02-12 21:09:36 -0500

    too easy

--- Type_of_Triangle.sql
+++
@@ -1,5 +0,0 @@
-select case when not (A+B > C AND B+C > A AND C+A > B) then 'Not A Triangle'
-       else (case when A=B AND B=C then 'Equilateral'
-                  when A=B or B=C or C=A THEN 'Isosceles'
-                  else 'Scalene' end) end
-from triangles 
